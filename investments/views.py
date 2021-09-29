1bdf58c2b7b90a75a6e1b72db81fd970906af1a4
Jungsoo Kim <hollibleling12@gmail.com>
2021-09-29 11:26:24 +0900

    feat : Detail page view get method added & db_uploader and csv file added
    chore : gitignore changed

--- investments/views.py
+++ investments/views.py
@@ -1,7 +1,8 @@
 from django.views import View
 from django.http import JsonResponse
 
-from .models import Investment
+from .models import Investment, Image
+
 
 class InvestmentListView(View):
     def get(self, request):
@@ -21,3 +22,49 @@ class InvestmentListView(View):
                 "image"            : investment.image_set.all().first().url,
                 "recrutement_rate" : int(investment.current_amount/investment.target_amount*100)
             } for investment in investments]}, status = 200)
+
+
+class InvestmentDetailView(View):
+    def get(self, request, investment_id):
+        if not Investment.objects.filter(id = investment_id).exists():
+            return JsonResponse({'MESSAGE' : 'NOT_FOUND'}, status = 404)
+
+        investment = Investment.objects.select_related("grade", "repayment_type", "detail", "security", "borrower").prefetch_related("image_set").get(id = investment_id)
+        images = investment.image_set.all()
+
+        return JsonResponse(
+            {
+                "image_list"             : [image.url for image in images], 
+                "id"                     : investment.id,
+                "name"                   : investment.name,
+                "grade"                  : investment.grade.name,
+                "return_rate"            : investment.return_rate,
+                "duration"               : investment.duration,
+                "repayment_types"        : investment.repayment_type.name, 
+                "current_amount"         : investment.current_amount, 
+                "target_amount"          : investment.target_amount,
+                "recrutement_rate"       : int(investment.current_amount/investment.target_amount*100), 
+                "LTV"                    : round(((investment.target_amount + investment.detail.priority_bond_amount)/investment.detail.evaluation_price)*100, 2), 
+                "repayment_day"          : investment.detail.repayment_day, 
+                "loan_type"              : investment.detail.loan_type.name, 
+                "evaluation_price"       : investment.detail.evaluation_price, 
+                "priority_bond_amount"   : investment.detail.priority_bond_amount, 
+                "security_surcharge"     : (investment.detail.evaluation_price - investment.detail.priority_bond_amount - investment.target_amount), 
+                "bidding_rate"           : investment.detail.bidding_rate, 
+                "expected_recovery"      : (investment.detail.evaluation_price * investment.detail.bidding_rate) - investment.detail.priority_bond_amount, 
+                "address"                : investment.security.address, 
+                "completion_date"        : investment.security.completion_date, 
+                "household"              : investment.security.household, 
+                "supply_area"            : investment.security.supply_area, 
+                "exclusive_private_area" : investment.security.exclusive_private_area, 
+                "lease_status"           : investment.security.lease_status, 
+                "latitude"               : investment.security.latitude, 
+                "longitude"              : investment.security.longitude, 
+                "credit_score"           : investment.borrower.credit_score, 
+                "income_type"            : investment.borrower.income_type, 
+                "income"                 : investment.borrower.income, 
+                "card_usage_amount"      : investment.borrower.card_usage_amount, 
+                "loan_amount"            : investment.borrower.loan_amount, 
+                "is_overdue"             : investment.borrower.is_overdue, 
+                "overdue_tax"            : investment.borrower.overdue_tax
+            }, status = 200)
