b2732ea979504f1537c0179763ea899a7239b0e8
Jungsoo Kim <hollibleling12@gmail.com>
2021-09-30 19:01:41 +0900

    feat : Transaction_information_view created
    chore : convention changed
    
    chore : conflict message deleted
    
    fix : Transaction_information_view changed
    
    fix : Transaction_information_view changed

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("/invest/<int:investment_id>", views.InvestTransactionView.as_view()),
     path("/deposit", views.DepositTransactionView.as_view()),
     path("/portfolio", views.PortfolioView.as_view()),
+    path("/transaction", views.TransactionInformationView.as_view()),
 ]
