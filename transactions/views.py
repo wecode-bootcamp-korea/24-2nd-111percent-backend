0bd3dbeb4b9a4b8e1ff11a4de34151cab0b7facc
YeJun <hayejun1013@naver.com>
2021-09-30 11:35:05 +0900

    fix: fix portfolio get method json format

--- transactions/views.py
+++ transactions/views.py
@@ -124,8 +124,10 @@ class PortfolioView(View):
                 "withdrawal_account": f"{deposit.withdrawal_bank.name}{deposit.withdrawal_account}",
                 "deposit_account": f"{deposit.deposit_bank.name}{deposit.deposit_account}",
                 "deposit_balance": deposit.balance,
-                "gross_investment_limit": 30000000 - portfolios.aggregate(assets=Sum("amounts"))["assets"],
-                "real_estate_investment_limit": 10000000 - portfolios.aggregate(assets=Sum("amounts"))["assets"]
+                "gross_investment_limit": 30000000
+                - portfolios.aggregate(assets=Sum("amounts"))["assets"],
+                "real_estate_investment_limit": 10000000
+                - portfolios.aggregate(assets=Sum("amounts"))["assets"],
             },
             "investment_general_infomation": {
                 "rate_of_return": portfolios.aggregate(
@@ -138,39 +140,117 @@ class PortfolioView(View):
                 )["cumulative_profit"],
             },
             "investment_current_condition": {
-                "investing_amount": portfolio_investings.aggregate(
-                    amounts=Sum("amounts")
-                )["amounts"],
-                "invest_completed_amount": portfolios.filter(
-                    investment_state_id=InvestmentState.State.COMPLETE.value
-                ).aggregate(amounts=Sum("amounts"))["amounts"],
-                "loss_amount": portfolios.filter(
-                    investment_state_id=InvestmentState.State.LOSS.value
-                ).aggregate(amounts=Sum("amounts"))["amounts"],
-                "investing_normal_amount": portfolio_investings.filter(
-                    repayment_state_id=RepaymentState.State.NORMAL.value
-                ).aggregate(amounts=Sum("amounts"))["amounts"],
-                "investing_delay_amount": portfolio_investings.filter(
-                    repayment_state_id=RepaymentState.State.DELAY.value
-                ).aggregate(amounts=Sum("amounts"))["amounts"],
-                "investing_overdue_amount": portfolio_investings.filter(
-                    repayment_state_id=RepaymentState.State.OVERDUE.value
-                ).aggregate(amounts=Sum("amounts"))["amounts"],
+                "all": [
+                    {
+                        "id": 1,
+                        "name": "investing_amount",
+                        "price": portfolio_investings.aggregate(amounts=Sum("amounts"))[
+                            "amounts"
+                        ],
+                    },
+                    {
+                        "id": 2,
+                        "name": "invest_completed_amount",
+                        "price": portfolios.filter(
+                            investment_state_id=InvestmentState.State.COMPLETE.value
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 3,
+                        "name": "loss_amount",
+                        "price": portfolios.filter(
+                            investment_state_id=InvestmentState.State.LOSS.value
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                ],
+                "investing": [
+                    {
+                        "id": 1,
+                        "name": "investing_normal_amount",
+                        "price": portfolio_investings.filter(
+                            repayment_state_id=RepaymentState.State.NORMAL.value
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 2,
+                        "name": "investing_delay_amount",
+                        "price": portfolio_investings.filter(
+                            repayment_state_id=RepaymentState.State.DELAY.value
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 3,
+                        "name": "investing_overdue_amount",
+                        "price": portfolio_investings.filter(
+                            repayment_state_id=RepaymentState.State.OVERDUE.value
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                ],
             },
             "portfolio_current_condition": {
-                "grade": {
-                    "A": portfolios.filter(investment__grade__name__contains="A").aggregate(amounts=Sum("amounts"))["amounts"],
-                    "B": portfolios.filter(investment__grade__name__contains="B").aggregate(amounts=Sum("amounts"))["amounts"],
-                    "C": portfolios.filter(investment__grade__name__contains="C").aggregate(amounts=Sum("amounts"))["amounts"],
-                    "D": portfolios.filter(investment__grade__name__contains="D").aggregate(amounts=Sum("amounts"))["amounts"],
-                },
-                "return_rate": {
-                    "8_under": portfolios.filter(investment__return_rate__lt=8).aggregate(amounts=Sum("amounts"))["amounts"],
-                    "8_over_or_equal": portfolios.filter(investment__return_rate__gte=8, investment__return_rate__lt=10).aggregate(amounts=Sum("amounts"))["amounts"],
-                    "10_over_or_equal": portfolios.filter(investment__return_rate__gte=10, investment__return_rate__lt=12).aggregate(amounts=Sum("amounts"))["amounts"],
-                    "12_over_or_equal": portfolios.filter(investment__return_rate__gte=12).aggregate(amounts=Sum("amounts"))["amounts"],
-                }
-            }
+                "grade": [
+                    {
+                        "id": 1,
+                        "name": "A",
+                        "price": portfolios.filter(
+                            investment__grade__name__contains="A"
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 2,
+                        "name": "B",
+                        "price": portfolios.filter(
+                            investment__grade__name__contains="B"
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 3,
+                        "name": "C",
+                        "price": portfolios.filter(
+                            investment__grade__name__contains="C"
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 4,
+                        "name": "D",
+                        "price": portfolios.filter(
+                            investment__grade__name__contains="D"
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                ],
+                "return_rate": [
+                    {
+                        "id": 1,
+                        "name": "8_under",
+                        "price": portfolios.filter(
+                            investment__return_rate__lt=8
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 2,
+                        "name": "8_over_or_equal",
+                        "price": portfolios.filter(
+                            investment__return_rate__gte=8,
+                            investment__return_rate__lt=10,
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 3,
+                        "name": "10_over_or_equal",
+                        "price": portfolios.filter(
+                            investment__return_rate__gte=10,
+                            investment__return_rate__lt=12,
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                    {
+                        "id": 4,
+                        "name": "12_over_or_equal",
+                        "price": portfolios.filter(
+                            investment__return_rate__gte=12
+                        ).aggregate(amounts=Sum("amounts"))["amounts"],
+                    },
+                ],
+            },
         }
 
         return JsonResponse({"results": results}, status=200)
