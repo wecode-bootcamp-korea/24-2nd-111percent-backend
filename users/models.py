bfb617b4f7bfecbc80be38ce02b1b0b3e8fc4446
YeJun <hayejun1013@naver.com>
2021-09-30 18:45:54 +0900

    feat: add kakao social login post method
    fix: fix signup post method logic
    
    
    fix: fix signup logic
    
    
    style: fix return message
    
    
    fix: fix social signin logic and test code
    
    
    fix: delete logging options

--- users/models.py
+++ users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class User(models.Model):
-    name         = models.CharField(max_length=32)
+    name         = models.CharField(max_length=32, null=True)
     email        = models.CharField(max_length=64, unique=True)
-    phone_number = models.CharField(max_length=32)
-    password     = models.CharField(max_length=128)
-    deposit      = models.ForeignKey("transactions.Deposit", on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=32, null=True)
+    password     = models.CharField(max_length=128, null=True)
+    deposit      = models.ForeignKey("transactions.Deposit", null=True, on_delete=models.PROTECT)
     kakao_id     = models.BigIntegerField(null=True)
 
     class Meta:
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,100 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from unittest.mock import patch, MagicMock
 
-# Create your tests here.
+
+class KakaoSocialLoginTest(TestCase):
+    @patch("users.kakao.requests")
+    def test_kakao_signin_post_success(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def __init__(self, status_code):
+                self.status_code = status_code
+
+            def json(self):
+                return {
+                    "id": 123456789,
+                    "kakao_account": {
+                        "profile": {
+                            "nickname": "최파란별멋쟁이",
+                        },
+                        "email": "cprb@naver.com",
+                    },
+                }
+
+        header = {"HTTP_Authorization": "Bearer test_kakao_access_token"}
+        mocked_requests.get = MagicMock(return_value=MockedResponse(200))
+        response = client.post(
+            "/users/signin/kakao", content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "message": "SUCCESS",
+                "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6M30.-mxXIixRm4tiNsBan9H6dYe3JsVOi2gbWxMSycVoGpc",
+                "user_name": "최파란별멋쟁이",
+                "user_email": "cprb@naver.com",
+            },
+        )
+
+    def test_kakao_signin_post_token_does_not_exist(self):
+        client = Client()
+
+        response = client.post("/users/signin/kakao", content_type="application/json")
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"message": "TOKEN_DOES_NOT_EXIST"})
+
+    @patch("users.kakao.requests")
+    def test_kakao_signin_email_required(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def __init__(self, status_code):
+                self.status_code = status_code
+
+            def json(self):
+                return {
+                    "id": 123456789,
+                    "kakao_account": {
+                        "profile": {
+                            "nickname": "최파란별멋쟁이",
+                        }
+                    },
+                }
+
+        header = {"HTTP_Authorization": "Bearer test_kakao_access_token"}
+        mocked_requests.get = MagicMock(return_value=MockedResponse(200))
+        response = client.post(
+            "/users/signin/kakao", content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.json(), {"message": "EMAIL_REQUIRED"})
+
+    @patch("users.kakao.requests")
+    def test_kakao_signin_profile_required(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def __init__(self, status_code):
+                self.status_code = status_code
+
+            def json(self):
+                return {
+                    "id": 123456789,
+                    "kakao_account": {
+                        "email": "cprb@naver.com",
+                    },
+                }
+
+        header = {"HTTP_Authorization": "Bearer test_kakao_access_token"}
+        mocked_requests.get = MagicMock(return_value=MockedResponse(200))
+        response = client.post(
+            "/users/signin/kakao", content_type="application/json", **header
+        )
+
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.json(), {"message": "PROFILE_REQUIRED"})
